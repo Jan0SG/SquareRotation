752211d79025d1bdba6bd3f28dbca7f108a4678c
Alejandro Sánchez González <114105548+Jan0SG@users.noreply.github.com>
2023-01-23 09:40:57 -0600

    Add files via upload

---
+++ Entregable 3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Entregable_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(156, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(485, 354);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RotateinCenter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(531, 407);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "RotateinOrigin";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(406, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "RotateinPivot";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Entregable 3/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entregable_3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        Point a, b, c, d;
+        Point A1, B2, C3, D4;
+        Point pivot, centerpoint;
+        Double angle;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            centerpoint = new Point(bmp.Width / 2 + 60, bmp.Height / 2 - 60);
+            pivot = new Point(bmp.Width/2, bmp.Height/2);
+
+            RenderCPlane();
+
+        }
+
+        private void RenderCPlane()
+        {
+            //Plano
+            a = new Point(pictureBox1.Width / 2, 0);
+            b = new Point(pictureBox1.Width / 2, pictureBox1.Height);
+            c = new Point(pictureBox1.Width, pictureBox1.Height / 2);
+            d = new Point(0, pictureBox1.Height / 2);
+
+            g.DrawLine(Pens.Yellow, a, b);
+            g.DrawLine(Pens.Yellow, c, d);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            RotateinCenter();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RotateinOrigin();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RotateinPivot();
+        }
+        private void RotateinCenter() //Rotates Square in its center
+        {
+            g.Clear(Color.Black);
+            RenderCPlane();
+            angle = Convert.ToInt16(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            a = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            b = new Point((pictureBox1.Width / 2) + 120, pictureBox1.Height / 2);
+            c = new Point((pictureBox1.Width / 2) + 120, (pictureBox1.Height / 2) - 120);
+            d = new Point(pictureBox1.Width / 2, (pictureBox1.Height / 2) - 120);
+
+            A1.X = (int)(((a.X - centerpoint.X) * Math.Cos(angle)) - ((a.Y - centerpoint.Y) * Math.Sin(angle)) + centerpoint.X);
+            A1.Y = (int)(((a.X - centerpoint.X) * Math.Sin(angle)) + ((a.Y - centerpoint.Y) * Math.Cos(angle)) + centerpoint.Y);
+            B2.X = (int)(((b.X - centerpoint.X) * Math.Cos(angle)) - ((b.Y - centerpoint.Y) * Math.Sin(angle)) + centerpoint.X);
+            B2.Y = (int)(((b.X - centerpoint.X) * Math.Sin(angle)) + ((b.Y - centerpoint.Y) * Math.Cos(angle)) + centerpoint.Y);
+            C3.X = (int)(((c.X - centerpoint.X) * Math.Cos(angle)) - ((c.Y - centerpoint.Y) * Math.Sin(angle)) + centerpoint.X);
+            C3.Y = (int)(((c.X - centerpoint.X) * Math.Sin(angle)) + ((c.Y - centerpoint.Y) * Math.Cos(angle)) + centerpoint.Y);
+            D4.X = (int)(((d.X - centerpoint.X) * Math.Cos(angle)) - ((d.Y - centerpoint.Y) * Math.Sin(angle)) + centerpoint.X);
+            D4.Y = (int)(((d.X - centerpoint.X) * Math.Sin(angle)) + ((d.Y - centerpoint.Y) * Math.Cos(angle)) + centerpoint.Y);
+
+            g.DrawLine(Pens.Red, A1, B2);
+            g.DrawLine(Pens.Red, B2, C3);
+            g.DrawLine(Pens.AliceBlue, C3, D4);
+            g.DrawLine(Pens.Red, D4, A1);
+
+            pictureBox1.Invalidate();
+
+        } 
+
+        private void RotateinOrigin() //Rotates the square at the center of the plane
+        {
+            g.Clear(Color.Black);
+            RenderCPlane();
+            angle = Convert.ToInt16(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            a = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            b = new Point((pictureBox1.Width / 2) + 120, pictureBox1.Height / 2);
+            c = new Point((pictureBox1.Width / 2) + 120, (pictureBox1.Height / 2) - 120);
+            d = new Point(pictureBox1.Width / 2, (pictureBox1.Height / 2) - 120);
+
+            A1.X = (int)(((a.X - 65 - pivot.X) * Math.Cos(angle)) - ((a.Y + 65 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            A1.Y = (int)(((a.X - 65 - pivot.X) * Math.Sin(angle)) + ((a.Y + 65 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            B2.X = (int)(((b.X - 65 - pivot.X) * Math.Cos(angle)) - ((b.Y + 65  - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            B2.Y = (int)(((b.X - 65  - pivot.X) * Math.Sin(angle)) + ((b.Y + 65  - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            C3.X = (int)(((c.X - 65  - pivot.X) * Math.Cos(angle)) - ((c.Y + 65 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            C3.Y = (int)(((c.X - 65  - pivot.X) * Math.Sin(angle)) + ((c.Y + 65 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            D4.X = (int)(((d.X - 65  - pivot.X) * Math.Cos(angle)) - ((d.Y + 65 - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            D4.Y = (int)(((d.X - 65 - pivot.X) * Math.Sin(angle)) + ((d.Y + 65 - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+
+            g.DrawLine(Pens.Red, A1, B2);
+            g.DrawLine(Pens.Red, B2, C3);
+            g.DrawLine(Pens.AliceBlue, C3, D4);
+            g.DrawLine(Pens.Red, D4, A1);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void RotateinPivot() //Rotates the square around the quadrants of the plain using the origin of the plain as pivot
+        {
+            g.Clear(Color.Black);
+            RenderCPlane();
+            angle = Convert.ToInt16(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            a = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            b = new Point((pictureBox1.Width / 2) + 120, pictureBox1.Height / 2);
+            c = new Point((pictureBox1.Width / 2) + 120, (pictureBox1.Height / 2) - 120);
+            d = new Point(pictureBox1.Width / 2, (pictureBox1.Height / 2) - 120);
+
+            A1.X = (int)(((a.X - pivot.X) * Math.Cos(angle)) - ((a.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            A1.Y = (int)(((a.X - pivot.X) * Math.Sin(angle)) + ((a.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            B2.X = (int)(((b.X - pivot.X) * Math.Cos(angle)) - ((b.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            B2.Y = (int)(((b.X - pivot.X) * Math.Sin(angle)) + ((b.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            C3.X = (int)(((c.X - pivot.X) * Math.Cos(angle)) - ((c.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            C3.Y = (int)(((c.X - pivot.X) * Math.Sin(angle)) + ((c.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+            D4.X = (int)(((d.X - pivot.X) * Math.Cos(angle)) - ((d.Y - pivot.Y) * Math.Sin(angle)) + pivot.X);
+            D4.Y = (int)(((d.X - pivot.X) * Math.Sin(angle)) + ((d.Y - pivot.Y) * Math.Cos(angle)) + pivot.Y);
+
+            g.DrawLine(Pens.Red, A1, B2);
+            g.DrawLine(Pens.Red, B2, C3);
+            g.DrawLine(Pens.AliceBlue, C3, D4);
+            g.DrawLine(Pens.Red, D4, A1);
+
+            pictureBox1.Invalidate();
+        }
+
+    }
+}
